1a62e11c8c0f6f2135443cb5048f7432ca868cdb
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-13 16:29:07 -0400

    fixing mixed declarations

--- main.h
+++ main.h
@@ -2,7 +2,18 @@
 #define MAIN_H
 
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define MAX_ARGS 10
+#define MAX_PATH_LENGTH 1024
 
 /* Function declarations */
 int command_exists(char *cmd);
@@ -11,5 +22,3 @@ void handle_cat(char *filename);
 void handle_echo(char **argv);
 
 #endif /* MAIN.H */
-
-
