c6483b9a94614fc8aa968e04f81c9a66f27b5b38
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-15 13:24:46 -0400

    trying task 2

--- main.h
+++ main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_ARGS 10
 #define MAX_PATH_LENGTH 1024
+#define MAX_INPUT_SIZE 1024
 
 /* Function declarations */
 int command_exists(char *cmd);
