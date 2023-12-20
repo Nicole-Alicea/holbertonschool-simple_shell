87147e354b5a91c92ad19acb45d2fe88f48c9266
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-12-20 17:40:49 -0400

    last night changes.

---
+++ concept-exercises/1-readline.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ *
+ */
+
+int main (void)
+{
+	char endOf_file = 1;
+
+	while (endOf_file == 1)
+	{
+		if (endOf_file)
+		{
+			//
+			//features into facts.
+			//lev. (CHRONOS AND SYNCROS).
+			endOf_file = 0;
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
