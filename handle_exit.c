0324847fe88d5ff0897dcf094eae56f905db204f
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-21 21:08:05 -0400

    fixing betty

--- handle_exit.c
+++ handle_exit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * handle_exit - Handles the "exit" comand
+ * @cmd: String representing the command to be checked
+ *
+ * Return: 0 if the command is "exit", 1 otherwise
+ */
+
 int handle_exit(char *cmd)
 {
 	if (strcmp(cmd, "exit") == 0)
